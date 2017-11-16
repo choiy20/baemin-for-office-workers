314ed75950db7e37925bd257dbf8051317e95497
choiy20 <yoojin.kelly.choi@gmail.com>
2017-11-16 08:59:18 +0800

    uploading sign-up part

--- baemin/urls.py
+++ baemin/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^partner/', include('partner.urls')), #다른 URL전체를 가져올 때 include를 씀
     url(r'^admin/', admin.site.urls),
 ]
