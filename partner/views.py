c5fe0f8066c53e5f94abe20d9e9ba60aee1bf70a
choiy20 <yoojin.kelly.choi@gmail.com>
2017-11-19 22:15:02 +0800

    Adding signup, login and logout

--- partner/views.py
+++ partner/views.py
@@ -1,20 +1,48 @@
+from django.contrib.auth import (
+    authenticate,
+    login as auth_login,
+    logout as auth_logout,
+)
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def index(request):
     ctx = {}
     return render(request, "index.html", ctx)
 
-def signup(request):
+
+def login(request):
+    ctx = {}
     if request.method == "GET":
         pass
-    elif request.method =="POST":
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect("/partner/")
+        else:
+            ctx.update({"error": "Invalid username or password "})
+
+    return render(request, "login.html", ctx)
+
+
+def signup(request):
+    ctx = {}
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         user = User.objects.create_user(username, email, password)
-        # print (username, email, password)
 
-    ctx = {}
     return render(request, "signup.html", ctx)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/partner/")
