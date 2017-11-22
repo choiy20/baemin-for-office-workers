781238b3d48615bb0c76f9b108af62de63a08411
choiy20 <yoojin.kelly.choi@gmail.com>
2017-11-22 09:03:01 +0800

    Adding "registration of company information"

--- partner/models.py
+++ partner/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Partner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Company Name",
+    )
+    contact = models.CharField(
+        max_length=50,
+        verbose_name="Contact Number",
+    )
+    address = models.CharField(
+        max_length=200,
+        verbose_name= "Company Address",
+    )
+    description = models.TextField(
+        max_length=200,
+        verbose_name="Company Description",
+    )
